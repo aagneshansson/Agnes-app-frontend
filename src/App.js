e2417f8b8470b51f1c999ab218203df3da1eae3e
Agnes Harrysson Hansson <aagneshansson@hotmail.com>
2021-01-30 08:13:34 +0100

    tried to comment out localstorage to see if that worked better

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { combineReducers } from "@reduxjs/toolkit";
-//import { configureStore } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
 
 import { createStore, applyMiddleware, compose } from 'redux'
@@ -15,33 +15,34 @@ import { Signup } from './Components/Signup.js'
 import { StatusMessage } from './Components/Statusmessage.js'
 import { Signin } from 'Components/Signin.js';
 
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+//const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const reducer = combineReducers({ user: user.reducer })
+const reducer = combineReducers({ user: user.reducer });
+const store = configureStore({ reducer });
 
-// Store the code 
-// 1) Retrieve the local storage and use it as out initial state
-const persistedStateJSON = localStorage.getItem("reduxStates");
+// // Store the code 
+// // 1) Retrieve the local storage and use it as out initial state
+// const persistedStateJSON = localStorage.getItem("reduxStates");
 
-let persistedState = {};
+// let persistedState = {};
 
-if (persistedStateJSON) {
-  persistedState = JSON.parse(persistedStateJSON);
-}
+// if (persistedStateJSON) {
+//   persistedState = JSON.parse(persistedStateJSON);
+// }
 
-// 2) Create the store using the initial state and retrieved state 
+// // 2) Create the store using the initial state and retrieved state 
 
-const store = createStore(
-  reducer,
-  persistedState,
-  composeEnhancer(applyMiddleware(thunk))
-);
+// const store = createStore(
+//   reducer,
+//   persistedState,
+//   composeEnhancer(applyMiddleware(thunk))
+// );
 
 
-// 3) Store the state in localstorage on ANY redux state change 
-store.subscribe(() => {
-  localStorage.setItem('reduxStates', JSON.stringify(store.getState()));
-});
+// // 3) Store the state in localstorage on ANY redux state change 
+// store.subscribe(() => {
+//   localStorage.setItem('reduxStates', JSON.stringify(store.getState()));
+// });
 
 export const App = () => {
   return (
