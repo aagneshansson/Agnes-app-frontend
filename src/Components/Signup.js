d674ce987bc567c87fcb714785d7b68d9818d849
Agnes Harrysson Hansson <aagneshansson@hotmail.com>
2021-02-12 12:49:28 +0100

    removing outcommented code

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
 import { signupfetch } from '../reducers/fetch';
 import { Mainwrapper, LightHeading, Form, Label, Input, RegisterButton } from '../Styling/Globalstyling';
